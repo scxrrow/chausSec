81e094e09fc5e5346b55b1ec614905412d52d0c1
scarrow <noahfelten@noahs-MacBook-Pro-2.local>
2026-02-26 14:51:26 +0100

    initial commit

---
+++ chausSecBackend/src/main/java/com/chausSec/controller/authenticationController.java
@@ -0,0 +1,8 @@
+package com.chausSec.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class authenticationController {
+    
+}
---
+++ chausSecBackend/src/test/java/com/chausSec/chausSec/ChausSecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chausSec.chausSec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChausSecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
