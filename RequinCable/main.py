5542427aa6cf3305de183cc3cc0e7f5f04247e50
Noxillya <jeong.dan@pm.me>
2026-02-28 17:58:55 +0100

    Ajout d'affichage du protocol et mode capture continu

--- RequinCable/main.py
+++ RequinCable/main.py
@@ -1,24 +1,32 @@
 #!/usr/bin/python3
 
 from scapy.all import sniff
-from scapy.layers.inet import IP
+from scapy.layers.inet import IP, TCP, UDP, ICMP
 
 
 def packet_callback(packet):
-
     if IP in packet:
         src = packet[IP].src
         dst = packet[IP].dst
 
-        print(f"{src} -> {dst}")
+        protocol = "OTHER"
+
+        if ICMP in packet:
+            protocol = "ICMP"
+
+        elif TCP in packet:
+            protocol = "TCP"
+
+        elif UDP in packet:
+            protocol = "UDP"
+
+        print(f"{src} -> {dst} ({protocol})")
 
 
 def main():
-
     sniff(
         prn=packet_callback,
-        store=False,
-        count=10
+        store=False
     )
 
 
