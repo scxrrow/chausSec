9aeb35fd6fbadefe09f51b9da1eac80123cdbf7e
Noxillya <jeong.dan@pm.me>
2026-02-28 17:02:51 +0100

    Capture et affichage de packets avec scapy

--- RequinCable/main.py
+++ RequinCable/main.py
@@ -1,5 +1,19 @@
+#!/usr/bin/python3
+
+from scapy.all import sniff
+
+
+def packet_callback(packet):
+    print(packet.summary())
+
+
 def main():
-    return
+    sniff(
+        prn=packet_callback,
+        store=False,
+        count=10
+    )
+
 
 if __name__ == "__main__":
     main()
