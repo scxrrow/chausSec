60f8a44b15a5070c96ba8bc7f0b7f53af478f012
scarrow <noahfelten@noahs-MacBook-Pro-2.local>
2026-03-12 12:16:14 +0100

    added dockerfile

---
+++ backend/src/main/java/com/chaussec/backend/DockerController.java
@@ -0,0 +1,15 @@
+package com.chaussec.backend;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class DockerController {
+    
+    @GetMapping("/docker")
+    public String getMethodName() {
+        return "Hi !";
+    }
+    
+}
